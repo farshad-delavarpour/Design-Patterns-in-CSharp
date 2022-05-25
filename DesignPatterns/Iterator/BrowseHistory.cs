ebcdccc95712abce90ec1a1d862fe5b2f8a92acf
Farshad Delavarpour <farshaddelavarpour@gmail.com>
2022-05-25 17:00:51 +0430

    Iterator Pattern

---
+++ DesignPatterns/Iterator/BrowseHistory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Iterator
+{
+    public class BrowseHistory
+    {
+        private List<string> Urls { get; } = new List<string>();
+        
+        public void Push(string url) =>
+            Urls.Add(url);
+
+        public string Pop()
+        {
+            var lastUrl = Urls.Last();
+            Urls.Remove(lastUrl);
+            return lastUrl;
+        }
+
+        public IIterator CreateIterator()
+        {
+            return new ListIterator(this);
+        }
+
+        public class ListIterator : IIterator
+        {
+            private readonly BrowseHistory _history;
+            private int _index;
+
+            public ListIterator(BrowseHistory history)
+            {
+                _history = history;
+            }
+
+            public string Current()
+            {
+                return _history.Urls[_index];
+            }
+
+            public bool HasNext()
+            {
+                return _index < _history.Urls.Count;
+            }
+
+            public void Next() => _index++;
+        }
+    }
+
+    
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿#region Iterator
+
+using DesignPatterns.Iterator;
+
+var history = new BrowseHistory();
+
+history.Push("a");
+history.Push("b");
+history.Push("c");
+
+var iterator = history.CreateIterator();
+
+while (iterator.HasNext())
+{
+    var url = iterator.Current();
+    Console.WriteLine(url);
+    iterator.Next();
+}
+
+Console.ReadKey();
+
+#endregion
